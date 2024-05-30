f08af7cf65533a2c725c5e769080a344e487073e
Marcelo-Rocha-UFF <marcelo_rochs@id.uff.br>
2024-05-30 18:33:05 -0300

    Version from the first demo live at MidiaCom Lab

--- eva-audio-module/eva-audio.py
+++ eva-audio-module/eva-audio.py
@@ -4,17 +4,18 @@ import subprocess
 
 import sys
 sys.path.append('/home/pi/EVA_ROBOT')
-import config # Módulo com as configurações dos dispositivos de rede.
+import config # Module with network device configurations.
 
-broker = config.MQTT_BROKER_ADRESS # Endereço do Broker.
-port = config.MQTT_PORT # Porta do Broker.
+broker = config.MQTT_BROKER_ADRESS # Broker address.
+port = config.MQTT_PORT # Broker Port.
 topic_base = config.EVA_TOPIC_BASE
 
 
-# 
+# Play a Sound or a Speech
 def playsound(file_path, audio_file, type, block = True):
         print(type)
         if type == "audio":
+            client.publish(topic_base + "/log", "Playing the sound: " + audio_file)
             audio_format = config.AUDIO_EXTENSION
         elif type == "speech":
             audio_format = config.WATSON_AUDIO_EXTENSION
@@ -30,7 +31,8 @@ def playsound(file_path, audio_file, type, block = True):
 
         
 
-# A fala é sempre do tipo (Blocking). A função playsound é responsável por colocar o estado do robô como "FREE".
+# The speech is always of the type (Blocking).
+# The playsound function is responsible for setting the robot's state to "FREE".
 def speech(audio_file, block = True):
         file_path = "eva-tts-module/tts_cache_files/"
         playsound(file_path, audio_file, "speech", block)
@@ -40,9 +42,8 @@ def speech(audio_file, block = True):
 # MQTT
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    #print("Connected with result code " + str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
-        # reconnect then subscriptions will be renewed.
+    # Reconnect then subscriptions will be renewed.
     client.subscribe(topic=[(topic_base + '/audio', 1), ])
     client.subscribe(topic=[(topic_base + '/speech', 1), ])
     print("Audio Module - Connected.")
@@ -61,10 +62,10 @@ def on_message(client, userdata, msg):
 
     if msg.topic == topic_base + '/speech':
         file_name = msg.payload.decode()
-        speech(file_name, True) # A fala roda sempre no modo "Blocking".
+        speech(file_name, True) # Speech always runs in "Blocking" mode.
 
 
-# Executa a thread do cliente MQTT.
+# Run the MQTT client thread.
 client = mqtt_client.Client()
 client.on_connect = on_connect
 client.on_message = on_message
